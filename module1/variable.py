691262460be9467abd862c29fcfefb67b52f3510
Jerry Paa Ekow Nyankson <jerrynyankson@softwaretechagency.com>
2022-10-29 16:46:07 -1200

    Added loops to the project

---
+++ module1/variable.py
@@ -0,0 +1,31 @@
+#varible
+
+#variable declaration
+#variable instatiation 
+
+#list
+from os import makedirs
+
+
+studentName = ["Jerry","Ella","Angel","Nana Kobina","Eugenia","Jerry"]
+studentName[5]= 10
+print(len(studentName))
+
+print(studentName[0:6])
+
+
+print("=================================================")
+
+
+#tuple
+marks = (10,12,11,23,24,21,10)
+print(len(marks))
+
+print(marks[3:7])
+
+print("=================================================")
+
+
+#Set
+set1 = {1,2,3,4,5,6,7,8,9,1}
+print(set1)
